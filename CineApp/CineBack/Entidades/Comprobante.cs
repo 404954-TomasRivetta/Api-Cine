a7697d0b63b5bd4990a59d499bcc8f8d6a8c4789
Alexis Cantonati <405634@tecnicatura.frc.utn.edu.ar>
2023-11-10 15:00:54 -0300

    Clases en la carpeta entidades

---
+++ CineApp/CineBack/Entidades/Comprobante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineBack.Entidades
+{
+    internal class Comprobante
+    {
+        public int id_cliente { get; set; }
+        public int id_forma_pago { get; set; }
+        public int id_empleado { get; set; }
+        public int cant_entradas { get; set; }
+        public List<Tickets> tickets { get; set; }
+        public Comprobante()
+        {
+            tickets = new List<Tickets>();
+        }
+        public void AgregarTicket(Tickets ticket)
+        {
+            tickets.Add(ticket);
+        }
+        public void QuitarTicket(int posicion)
+        {
+            tickets.RemoveAt(posicion);
+        }
+    }
+}
---
+++ CineApp/CineBack/Entidades/Pelicula.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineBack.Entidades
+{
+    internal class Pelicula
+    {
+        public string descripcion { get; set; }
+        public int id_tipo_pelicula { get; set; }
+        public int id_idioma { get; set; }
+        public int id_tipo_publico { get; set; }
+        public Boolean subtitulada { get; set; }
+        public int id_director { get; set; }
+        public Pelicula()
+        {
+            descripcion = string.Empty;
+            id_tipo_pelicula = 0;
+            id_idioma = 0;
+            id_tipo_publico = 0;
+            subtitulada = false;
+            id_director = 0;
+        }
+        public Pelicula(string des,int tipo_pel,int idioma,int tipo_pub,Boolean sub,int dir)
+        {
+            descripcion=des;
+            id_tipo_pelicula=tipo_pel;
+            id_idioma=idioma;
+            id_tipo_publico=tipo_pub;
+            subtitulada=sub;
+            id_director=dir;
+        }
+    }
+}
