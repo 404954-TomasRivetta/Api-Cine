4abd306b596b31e7572ac4128e79f3a4ac91fec4
valentino113991 <113991@tecnicatura.frc.utn.edu.ar>
2023-11-12 18:33:03 -0300

    form acerca de

---
+++ CineApp/CineFront/Presentacion/Formularios/FrmAcercaDe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CineFront.Presentacion.Formularios
+{
+    public partial class FrmAcercaDe : Form
+    {
+        public FrmAcercaDe()
+        {
+            InitializeComponent();
+        }
+    }
+}
