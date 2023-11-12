e768b01233634d4460b74e362709db828587009b
404954-TomasRivetta <404954@tecnicatura.frc.utn.edu.ar>
2023-11-12 19:06:54 -0300

    creando clienteSingleton y reestructurando

---
+++ CineApp/CineFront/Servicio/ClienteSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineFront.Servicio
+{
+    public class ClienteSingleton
+    {
+        private static ClienteSingleton instancia;
+
+        private HttpClient client;
+
+        private ClienteSingleton()
+        {
+            client = new HttpClient();
+        }
+
+        public static ClienteSingleton getInstance()
+        {
+
+            if (instancia == null)
+            {
+                instancia = new ClienteSingleton();
+            }
+            return instancia;
+
+        }
+
+
+    }
+}
