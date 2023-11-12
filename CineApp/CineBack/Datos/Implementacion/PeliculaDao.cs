d4161b26825dcab296b9f399eceda1d966a5006a
valentino113991 <113991@tecnicatura.frc.utn.edu.ar>
2023-11-12 17:29:33 -0300

    Cosas generales

---
+++ CineApp/CineBack/Datos/Implementacion/PeliculaDao.cs
@@ -0,0 +1,15 @@
+﻿using CineBack.Datos.Interfaz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineBack.Datos.Implementacion
+{
+    public class PeliculaDao: IPeliculaDao
+    {
+        //Completar...
+
+    }
+}
