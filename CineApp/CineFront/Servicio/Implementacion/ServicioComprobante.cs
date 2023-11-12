602e5143117cb4648e5cf8debeaf31ab55efa149
valentino113991 <113991@tecnicatura.frc.utn.edu.ar>
2023-11-12 18:00:10 -0300

    Pequeños cimientos del front

---
+++ CineApp/CineFront/Servicio/Implementacion/ServicioComprobante.cs
@@ -0,0 +1,14 @@
+﻿using CineFront.Servicio.Interfaz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineFront.Servicio.Implementacion
+{
+    public class ServicioComprobante: IServicioComprobante
+    {
+        //Completar...
+    }
+}
